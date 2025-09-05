f6997975df0e12d05f8b2af835b3aebd23f17415
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-09-05 00:26:18 -0400

    finish script

--- script.py
+++ script.py
@@ -2,18 +2,39 @@ import smtplib
 from email.message import EmailMessage
 import os
 from dotenv import load_dotenv
+import csv
+from datetime import datetime
 
-load_dotenv()
+def main():
+   
 
-EMAIL_ADDRESS = f'{os.getenv('EMAIL')}'
-EMAIL_PASSWORD = f'{os.getenv('PASSWORD')}'
+    today = datetime.today()
+    today_month_day = (today.month, today.day)
+    today_month_day_string = str(today_month_day[0]) + '-' + str(today_month_day[1])
+    birthday_person = False
 
-msg = EmailMessage()
-msg['Subject'] = 'Birthday Alert!'
-msg['From'] = EMAIL_ADDRESS
-msg['To'] = EMAIL_ADDRESS
-msg.set_content('Hello Caleb! This is a test')
+    load_dotenv()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    smtp.send_message(msg)
+    with open(f'{os.getenv('CSV_PATH')}', mode ='r')as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            if lines[1] == today_month_day_string:
+                birthday_person = lines[0]
+            
+    if birthday_person:
+    
+        EMAIL_ADDRESS = f'{os.getenv('EMAIL')}'
+        EMAIL_PASSWORD = f'{os.getenv('PASSWORD')}'
+
+        msg = EmailMessage()
+        msg['Subject'] = 'Birthday Alert!'
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = EMAIL_ADDRESS
+        msg.set_content(f"Hello Caleb! Here is a reminder that it is {birthday_person}'s birthday!")
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+
+if __name__ == "__main__":
+    main()
